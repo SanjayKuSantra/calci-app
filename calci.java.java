e86af046e96b4611d1949ebd88a4211e2f05db47
Sanjay Kumar Santra <sanjaykumar.santra02@gmail.com>
2024-05-17 15:32:35 +0530

    message

---
+++ calci.java.java
@@ -0,0 +1,12 @@
+public class EqualOrNot {
+	public static void main(String[] args) {
+		int firstNumber = 15;
+		int secondNumber = 15;
+		
+		if ((firstNumber - secondNumber) == 0) {
+			System.out.println("Number are equal");
+		} else {
+			System.out.println("Number are not equal");
+		}
+	}
+}
